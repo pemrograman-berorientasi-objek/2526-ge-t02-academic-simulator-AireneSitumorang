718bc57611529fb155a5c63f83301753ae64418e
AireneSitumorang <irensitumorang2020@gmail.com>
2026-03-03 10:19:25 +0700

    sudah menyelesaikan

--- src/academic/model/Student.java
+++ src/academic/model/Student.java
@@ -1,11 +1,37 @@
 package academic.model;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
- */
 public class Student {
+    private String nim;
+    private String name;
+    private int entranceYear; // Tahun Angkatan
+    private String major;     // Jurusan
 
-    // class definition
+    public Student(String nim, String name, int entranceYear, String major) {
+        this.nim = nim;
+        this.name = name;
+        this.entranceYear = entranceYear;
+        this.major = major;
+    }
 
+    public String getNim() {
+        return nim;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getEntranceYear() {
+        return entranceYear;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    // Metode toString ini akan membantu dalam menampilkan data dengan format yang diinginkan
+    @Override
+    public String toString() {
+        return nim + "|" + name + "|" + entranceYear + "|" + major;
+    }
 }
